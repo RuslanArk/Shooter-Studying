a660784e01cec7ade0b3fc81ffe71885e7cb08ef
Ruslan Kornis <RKornis@users.noreply.github.com>
2021-06-15 14:40:54 +0300

    WeaponChangeService

---
+++ Source/ShootThemUp/Private/AI/Services/STUChangeWeaponService.cpp
@@ -0,0 +1,28 @@
+// Shoot Them Up, All right Reserved
+
+
+#include "AI/Services/STUChangeWeaponService.h"
+
+#include "AIController.h"
+#include "Components/STUWeaponComponent.h"
+#include "ShootThemUp/STUUtils.h"
+
+USTUChangeWeaponService::USTUChangeWeaponService()
+{
+	NodeName = "Change Weapon";
+}
+
+void USTUChangeWeaponService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	const auto Controller = OwnerComp.GetAIOwner();
+	if (Controller)
+	{
+		const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(Controller->GetPawn());
+		if (WeaponComponent && Probability > 0 && FMath::FRand() <= Probability)
+		{
+			WeaponComponent->NextWeapon();
+		}
+	}
+	
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
