6e88cfafa4ed392f7144f4b5d96d28c0cbac8c53
Ruslan Kornis <RKornis@users.noreply.github.com>
2021-06-19 18:38:30 +0300

    Game Mode: Spawn Bots

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -83,3 +83,14 @@ struct FImpactData
 	FDecalData DecalData;
 	
 };
+
+// GameMode
+
+USTRUCT(BlueprintType)
+struct FGameData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin = "1", ClampMax = "100"))
+	int32 PlayersNum = 2;
+};
